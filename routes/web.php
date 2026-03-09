a5ec5af77a12486da8bdde36a5dc001fe7e026a2
WildCode <m.sadakhanov@skoleom.com>
2026-03-09 16:30:44 +0100

    8

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\ArticleController;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');
+
+Route::fallback(function () {
+    return view('errors.not-found');
+});
