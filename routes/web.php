792a53badbd4e77db8807bd33b0f2d4f37034bb7
WildCode <m.sadakhanov@skoleom.com>
2026-03-09 16:28:10 +0100

    7

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [IndexController::class, 'index']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::get('/', [IndexController::class, 'index'])->name('home');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');
