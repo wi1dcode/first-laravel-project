9fe2a995a0a531bc1e72796dee755ef76c5dcdc3
WildCode <m.sadakhanov@skoleom.com>
2026-03-10 09:59:29 +0100

    10 fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,3 @@
 <header>
     <h1>Mon site Laravel</h1>
-    <nav>
-        <a href="/">Accueil</a>
-        <a href="/articles/1">Article</a>
-        <a href="/contact">Contact</a>
-    </nav>
 </header>
