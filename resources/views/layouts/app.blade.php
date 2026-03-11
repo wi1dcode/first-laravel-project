b4f98c73d53e33d49e4a9bafc5a2a90140984400
WildCode <m.sadakhanov@skoleom.com>
2026-03-11 10:20:02 +0100

    16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <title>@yield('title')</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     @include('partials.header') 
